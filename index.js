bab8eefe8d509665ae05a1cfda2674842b875adb
kiso <kiso>
2016-03-10 09:54:57 +0900

    add uibDatepickerPopupConfig

--- index.js
+++ index.js
@@ -3,14 +3,17 @@
     angular.module(myApp, ['ui.bootstrap']);
     
     // config
-    // angular.module(myApp)
-    // .config(function (datepickerPopupConfig) {
+    angular.module(myApp)
+    .config(['uibDatepickerPopupConfig',function (datepickerPopupConfig) {
 
-    //     angular.extend(datepickerPopupConfig, {
-    //         currentText: '今日',
-    //         clearText: 'クリア',
-    //         closeText: '閉じる'
-    //     });
+        angular.extend(datepickerPopupConfig, {
+        currentText: '今日',
+        clearText: '元に戻す',
+        closeText: '閉じる',
+        formatDayTitle:'yyyy年MMMM',
+        showWeeks: false,
 
-    // });
+        });
+
+    }]);
 })();
